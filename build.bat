7accfed247f34acc4ef31327745362b46581d9f0
Ben <benjaminwmoore91@gmail.com>
2022-03-20 00:01:45 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-
+if not exist ./glm (
+    git clone https://github.com/g-truc/glm
+)
 %VULKAN_SDK%/bin/glslc shader.vert -o vert.spv
 %VULKAN_SDK%/bin/glslc shader.frag -o frag.spv
 cmake -S . -B build/ . -G "MinGW Makefiles"
